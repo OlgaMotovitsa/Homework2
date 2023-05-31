da3a96d173139fc0bc1abeb127414cc0087618d4
DFIT Ольга <Olga.Motovitsa@gmail.com>
2023-05-31 13:54:13 +0300

    add homework2 changes

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,2 +1,18 @@
+
 public class Main {
+    public static void main(String[] args) {
+
+//        Task3 task3 = new Task3();
+//        task3.StringRequest();
+
+        Task1 task1 = new Task1();
+        task1.StringRequest();
+
+
+
+
+    }
+
 }
+
+
